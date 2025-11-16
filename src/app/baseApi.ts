46951cc0ae7ff9b21ccb2e9f27003a1fa2d14f6b
Elizabeth Kritskaya <post.kri@gmail.com>
2025-11-16 13:31:05 +0300

    add SkeletonTodolist

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -2,15 +2,20 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {AUTH_TOKEN} from "@/common/constants";
 
 export const baseApi = createApi({
-    baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_BASE_URL,
-        headers: {
-            "API-KEY": import.meta.env.VITE_API_KEY,
-        },
-        prepareHeaders: (headers, _api) => {
-            headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
-        },
-    }),
+    baseQuery: async(args, api, extraOptions) => {
+
+        await new Promise(res => setTimeout(res, 2000))
+
+        return fetchBaseQuery({
+            baseUrl: import.meta.env.VITE_BASE_URL,
+            headers: {
+                "API-KEY": import.meta.env.VITE_API_KEY,
+            },
+            prepareHeaders: (headers, _api) => {
+                headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+            },
+        })(args, api, extraOptions)
+    },
     reducerPath: "baseApi",
     tagTypes: ['Todolist', 'Task'],
     endpoints:() => ({})
--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -2,8 +2,15 @@ import Grid from "@mui/material/Grid"
 import Paper from "@mui/material/Paper"
 import {TodolistItem} from "./TodolistItem/TodolistItem"
 import {useFetchTodolistsQuery} from "@/features/todolists/api/todolistsApi.ts";
+import Box from "@mui/material/Box";
+import {containerSx} from "@/common/styles";
+import {TodolistSkeleton} from "@/features/todolists/ui/Todolists/TodolistSkeleton/TodolistSkeleton.tsx";
 
 export const Todolists = () => {
+
+  const {data, isLoading} = useFetchTodolistsQuery()
+
+
   // const todolists = useAppSelector(selectTodolists)
   //
   // const dispatch = useAppDispatch()
@@ -12,7 +19,20 @@ export const Todolists = () => {
   //   dispatch(fetchTodolistsTC())
   // }, [])
 
-  const {data, refetch} = useFetchTodolistsQuery()
+  if (isLoading) {
+    return (
+        <Box sx={containerSx} style={{ gap: "32px" }}>
+          {Array(3)
+              .fill(null)
+              .map((_, id) => (
+                  <TodolistSkeleton key={id} />
+              ))}
+        </Box>
+    )
+  }
+
+
+
 
   //console.log(data)
   // const [trigger, {data}] = useLazyFetchTodolistsQuery()
@@ -22,6 +42,7 @@ export const Todolists = () => {
     <>
       {/*<button onClick={refetch}>actual</button>*/}
       {/*<button onClick={fetchTodosHandler}>get todos</button>*/}
+      {/*<Skeleton variant={'circular'} width={40} height={40}/>*/}
       {data?.map((todolist: any) => (
         <Grid key={todolist.id}>
           <Paper sx={{ p: "0 20px 20px 20px" }}>
